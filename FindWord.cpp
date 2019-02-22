d5e109d268f5f5a71ab443c3ccc60bfb39c5d664
jyzhao19 <47873556+jyzhao19@users.noreply.github.com>
2019-02-21 18:21:46 -0800

    Update FindWord.cpp

--- FindWord.cpp
+++ FindWord.cpp
@@ -6,54 +6,59 @@ using namespace std;
 
 enum CommentStart
 {
-  CODE = 0,
-  LINE_CMT,
-  BLOCK_CMT,
-  STRING,
+    CODE = 0,
+    LINE_CMT,
+    BLOCK_CMT,
+    STRING,
 };
 
-class FindWord
+class Solution
 {
 public:
-  FindWord(string filename, string word);
+    bool FindWord(string filename, const string& word);
 private:
 };
 
-FindWord::FindWord(string filename, const string TheWord) {
-  const int TheWordLength = TheWord.length();
-  ifstream ifs(filename, ifstream::in);
-  string line;
-  
-  CommentStart cs = CODE;
-  while(ifs) {
-    getline(ifs, line);
-    
-    for (int i=0; i<line.length()-1; i++) {
-      if (line[i] == '"') {
-        switch (cs) {
-          case STRING:
-            cs = CODE; break;
-          case CODE:
-            cs = STRING; break;
-        }
-      }
-      
-      string s = line.substr(i, 2);
-      switch (s) {
-        case "//":
-          cs = LINE_CMT; break;
-        case "/*":
-          cs = BLOCK_CMT; break;
-        case "*/":
-          cs = CODE; break;
-      }
+bool Solution::FindWord(string filename, const string& TheWord) {
+    const int TheWordLength = TheWord.length();
+    ifstream ifs(filename, ifstream::in);
+    string line;
 
-      if (cs == LINE_CMT || cs == BLOCK_CMT) {
-        if (i<line.length()-TheWordLength && line.substr(i, 6) == TheWord) {
-          return true;
+    CommentStart cs = CODE;
+    while (ifs) {
+        getline(ifs, line);
+
+        for (int i = 0; i < line.length() - 1; i++) {
+            if (line[i] == '"') {
+                switch (cs) {
+                case STRING:
+                    cs = CODE; break;
+                case CODE:
+                    cs = STRING; break;
+                }
+            }
+
+            string s = line.substr(i, 2);
+            if (s == "//")
+                cs = LINE_CMT;
+            else if (s == "/*")
+                cs = BLOCK_CMT;
+            else if (s == "*/")
+                cs = CODE;
+
+            if (cs == LINE_CMT || cs == BLOCK_CMT) {
+                if (i < line.length() - TheWordLength && line.substr(i, 6) == TheWord) {
+                    return true;
+                }
+            }
         }
-      }
     }
-  }
-  return false;
+    return false;
+}
+
+int main()
+{
+    Solution s = Solution();
+    bool ret = s.FindWord("c:\\Users\\tony.zhao2\\Desktop\\dfd.txt", "GOOGLE"); // GOOGLE 
+    cout << ret << endl;
 }
