2f0f3972dff03d7cef9844cc596a38066f12160c
jyzhao19 <47873556+jyzhao19@users.noreply.github.com>
2019-02-24 23:14:42 -0800

    Update TOMORROW_LC560.cpp

--- TOMORROW_LC560.cpp
+++ TOMORROW_LC560.cpp
@@ -1,1 +1,17 @@
-
+class Solution {
+public:
+    int subarraySum(vector<int>& nums, int k) {
+        int cur_sum = 0;
+        unordered_map<int, int> d_{{0,1}};
+        
+        int res=0;
+        for (int i=0; i<nums.size(); i++) {
+            cur_sum+=nums[i];
+            auto it = d_.find(cur_sum-k);
+            if (it!=d_.end())
+              res+=it->second;
+            d_[cur_sum]==0?d_[cur_sum]=1:d_[cur_sum]++;
+        }
+        return res;
+    }
+};
