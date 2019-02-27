850b9df0ad0f59a07ea9c9a5039bd13bcc90abbb
jyzhao19 <47873556+jyzhao19@users.noreply.github.com>
2019-02-26 18:30:17 -0800

    Update LC236_V2.cpp

--- LC236_V2.cpp
+++ LC236_V2.cpp
@@ -10,17 +10,37 @@
 class Solution {
 public:
     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        // using a dict to store parent node of any node, then u can easily trace them back
+      unordered_map<TreeNode*, TreeNode*> m;                
       stack<TreeNode* > s;
       s.push(root);
-      
-      while (!s.empty()) {
-        auto n = s.top(); s.pop();
-        if (s->left)
-          s.push(s->left);
-        if (s->right)
-          s.push(s->right);
-          
+      m[root] = NULL;
         
+      while (!m.count(p) || !m.count(q)) {
+        auto n = s.top(); s.pop();
+        if (n->left) {
+          s.push(n->left);
+          m[n->left] = n;
+        }
+        if (n->right) {
+          s.push(n->right);
+          m[n->right] = n;
+        }
       }
+      
+      unordered_set<TreeNode*> pPath;
+      TreeNode* pp = p;
+      while (pp) {
+        pPath.insert(pp);
+        pp = m[pp];
+      }
+        
+      TreeNode* qq = q;
+      while (qq) {
+        if (pPath.count(qq))
+            break;
+        qq = m[qq];
+      }
+      return qq;
     }
 };
